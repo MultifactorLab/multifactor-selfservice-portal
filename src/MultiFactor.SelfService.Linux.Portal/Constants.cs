fd743d7b91d181d2c873fc0403f937e7b2dac738
Aleksey Pashkov <a.pashkov@multifactor.ru>
2022-07-18 18:13:11 +0300

    initial commit: templates, models, resources, web-static

---
+++ src/MultiFactor.SelfService.Linux.Portal/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiFactor.SelfService.Linux.Portal
+{
+    public class Constants
+    {
+        public const string COOKIE_NAME = "multifactor";
+        public const string SESSION_EXPIRED_PASSWORD_USER_KEY = "multifactor:expired-password:user";
+        public const string SESSION_EXPIRED_PASSWORD_CIPHER_KEY = "multifactor:expired-password:cipher";
+    }
+}
---
+++ src/MultiFactor.SelfService.Linux.Portal/Models/ExchangeActiveSyncDevice.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace MultiFactor.SelfService.Linux.Portal.Models
+{
+    public class ExchangeActiveSyncDevice
+    {
+        public string MsExchDeviceId { get; set; }
+        public string FriendlyName { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public DateTime WhenCreated { get; set; }
+        public ExchangeActiveSyncDeviceAccessState AccessState { get; set; }
+        public string AccessStateName
+        {
+            get
+            {
+                switch (AccessState)
+                {
+                    case ExchangeActiveSyncDeviceAccessState.Allowed:
+                        return Resources.ExchangeActiveSyncDevices.AllowedState;
+                    case ExchangeActiveSyncDeviceAccessState.Blocked:
+                        return Resources.ExchangeActiveSyncDevices.BlockedState;
+                    case ExchangeActiveSyncDeviceAccessState.Quarantined:
+                        return Resources.ExchangeActiveSyncDevices.QuarantinedState;
+                    case ExchangeActiveSyncDeviceAccessState.TestActiveSyncConnectivity:
+                        return "TestActiveSyncConnectivity";
+                    default:
+                        throw new NotImplementedException(AccessState.ToString());
+                }
+            }
+        }
+        public string AccessStateReason { get; set; }
+
+        /// <summary>
+        /// FriendlyName / Model / Type
+        /// </summary>
+        public string DisplayName
+        {
+            get
+            {
+                return new[]
+                {
+                    FriendlyName, Model, Type
+                }
+                .Where(el => !string.IsNullOrEmpty(el))
+                .Distinct()
+                .Aggregate((a, b) => a + " / " + b);
+            }
+        }
+    }
+
+    //https://social.technet.microsoft.com/Forums/en-US/016ed5b5-d26d-48e7-8c1f-613de81e6d91/how-can-i-use-the-shell-to-get-a-list-of-all-quarantined-devices-in-2010-sp1?forum=exchangesvrmobilitylegacy
+    public enum ExchangeActiveSyncDeviceAccessState
+    {
+        Allowed = 1,
+        Blocked = 2,
+        Quarantined = 3,
+        TestActiveSyncConnectivity = 4
+    }
+}
