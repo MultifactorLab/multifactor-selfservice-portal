d2c3d0a3dd749e261820b8b67e0467316dd62ea7
Aleksey Pashkov <a.pashkov@multifactor.ru>
2022-07-18 18:13:11 +0300

    initial commit: templates, models, resources, web-static

---
+++ src/MultiFactor.SelfService.Linux.Portal/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiFactor.SelfService.Linux.Portal.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources.Validation))]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources.Validation))]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Correct document URL from browser if we behind nginx or other proxy
+        /// </summary>
+        public string MyUrl { get; set; }
+    }
+}
