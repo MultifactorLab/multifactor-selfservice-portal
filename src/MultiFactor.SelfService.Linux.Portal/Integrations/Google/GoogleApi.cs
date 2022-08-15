eea7da2135427c18236cfe2e62c6daaf0a25913f
Aleksey Pashkov <a.pashkov@multifactor.ru>
2022-08-15 16:56:16 +0300

    added GoogleApi service; added captcha story;

---
+++ src/MultiFactor.SelfService.Linux.Portal/Integrations/Google/GoogleApi.cs
@@ -0,0 +1,25 @@
+﻿using MultiFactor.SelfService.Linux.Portal.Integrations.Google.Dto;
+
+namespace MultiFactor.SelfService.Linux.Portal.Integrations.Google
+{
+    public class GoogleApi
+    {
+        private readonly HttpClient _client;
+
+        public GoogleApi(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        /// <summary>
+        /// Verifies the user's reCAPTCHA response.
+        /// </summary>
+        /// <param name="secret">Required. The shared key between your site and reCAPTCHA.</param>
+        /// <param name="response">Required. The user response token provided by the reCAPTCHA client-side integration on your site.</param>
+        /// <param name="remoteip">Optional. The user's IP address.</param>
+        public Task<VerifyCaptchaResponseDto> VerifyCaptchaAsync(string secret, string response, string? remoteip = null)
+        {
+            return Task.FromResult(default (VerifyCaptchaResponseDto));
+        }
+    }
+}
---
+++ src/MultiFactor.SelfService.Linux.Portal/Stories/VerifyCaptchaStory/VerifyCaptchaStory.cs
@@ -0,0 +1,19 @@
+﻿using MultiFactor.SelfService.Linux.Portal.Integrations.Google;
+
+namespace MultiFactor.SelfService.Linux.Portal.Stories.VerifyCaptchaStory
+{
+    public class VerifyCaptchaStory
+    {
+        private readonly GoogleApi _googleApi;
+
+        public VerifyCaptchaStory(GoogleApi googleApi)
+        {
+            _googleApi = googleApi ?? throw new ArgumentNullException(nameof(googleApi));
+        }
+
+        public async Task ExecuteAsync()
+        {
+
+        }
+    }
+}
