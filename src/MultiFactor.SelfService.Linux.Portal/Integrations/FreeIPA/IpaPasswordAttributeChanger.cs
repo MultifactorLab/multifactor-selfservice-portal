840fbbea1be5320b3669101b5c5000c8a3c7b568
Aleksey Pashkov <56426436+apashkov-ext@users.noreply.github.com>
2023-03-06 21:17:53 +0600

    fallback for LDAP metadata reader was added (#40)

---
+++ src/MultiFactor.SelfService.Linux.Portal/Integrations/FreeIPA/IpaPasswordAttributeChanger.cs
@@ -0,0 +1,35 @@
+﻿using LdapForNet;
+using LdapForNet.Native;
+using MultiFactor.SelfService.Linux.Portal.Abstractions.Ldap;
+using MultiFactor.SelfService.Linux.Portal.Integrations.Ldap.Connection;
+using static LdapForNet.Native.Native;
+
+namespace MultiFactor.SelfService.Linux.Portal.Integrations.FreeIPA
+{
+    public class IpaPasswordAttributeChanger : IPasswordAttributeChanger
+    {
+        public async Task ExecuteChangeCommandAsync(string dn, string oldPassword, 
+            string newPassword, LdapConnectionAdapter connection)
+        {
+            var oldPasswordAttribute = new DirectoryModificationAttribute
+            {
+                Name = "userpassword",
+                LdapModOperation = LdapModOperation.LDAP_MOD_DELETE
+            };
+            oldPasswordAttribute.Add(oldPassword);
+
+            var newPasswordAttribute = new DirectoryModificationAttribute
+            {
+                Name = "userpassword",
+                LdapModOperation = Native.LdapModOperation.LDAP_MOD_ADD
+            };
+            newPasswordAttribute.Add(newPassword);
+
+            var response = await connection.SendRequestAsync(new ModifyRequest(dn, oldPasswordAttribute, newPasswordAttribute));
+            if (response.ResultCode != ResultCode.Success)
+            {
+                throw new Exception($"Password change command error: {response.ErrorMessage}");
+            }
+        }
+    }
+}
--- src/MultiFactor.SelfService.Linux.Portal/Integrations/Ldap/Connection/LdapServerInfo.cs
+++ src/MultiFactor.SelfService.Linux.Portal/Integrations/Ldap/Connection/LdapServerInfo.cs
@@ -4,10 +4,16 @@
     {
         public LdapImplementation Implementation { get; }
 
+        public static LdapServerInfo Default => new(LdapImplementation.ActiveDirectory);
+
         public LdapServerInfo(LdapImplementation implementation)
         {
             Implementation = implementation;
         }
 
+        public override string ToString()
+        {
+            return Implementation.ToString();
+        }
     }
 }
