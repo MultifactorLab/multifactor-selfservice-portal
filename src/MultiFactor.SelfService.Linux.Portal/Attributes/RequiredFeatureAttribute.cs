2b25b014df82df2eb5f0fbfb213c64506517b063
Oleg Brezhnev <oleg-423@yandex.ru>
2023-03-14 14:52:32 +0300

    Password managment features transfer initial commit

---
+++ src/MultiFactor.SelfService.Linux.Portal/Attributes/RequiredFeatureAttribute.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using MultiFactor.SelfService.Linux.Portal.Exceptions;
+using MultiFactor.SelfService.Linux.Portal.Extensions;
+using MultiFactor.SelfService.Linux.Portal.Settings;
+using System.ComponentModel;
+
+namespace MultiFactor.SelfService.Linux.Portal.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class RequiredFeatureAttribute : ActionFilterAttribute
+    {
+        private readonly ApplicationFeature _requiredFeatureFlags;
+
+        public RequiredFeatureAttribute(ApplicationFeature requiredFeatureFlags)
+        {
+            _requiredFeatureFlags = requiredFeatureFlags;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var httpContext = context.HttpContext;
+            var configuration = httpContext.RequestServices.GetRequiredService<PortalSettings>();
+            if (_requiredFeatureFlags.HasFlag(ApplicationFeature.PasswordManagement) && !configuration.EnablePasswordManagement)
+            {
+                throw new FeatureNotEnabledException(ApplicationFeature.PasswordManagement.GetEnumDescription());
+            }
+
+            if (_requiredFeatureFlags.HasFlag(ApplicationFeature.ExchangeActiveSyncDevicesManagement) && !configuration.EnableExchangeActiveSyncDevicesManagement)
+            {
+                throw new FeatureNotEnabledException(ApplicationFeature.ExchangeActiveSyncDevicesManagement.GetEnumDescription());
+            }
+        }
+    }
+
+    [Flags]
+    public enum ApplicationFeature
+    {
+        None = 0,
+
+        [Description("Password Management")]
+        PasswordManagement = 1 << 0,
+        
+        [Description("Exchange Acitve Sync Device Management")]
+        ExchangeActiveSyncDevicesManagement = 1 << 1
+    }
+}
---
+++ src/MultiFactor.SelfService.Linux.Portal/Filters/FeatureNotEnabledExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using MultiFactor.SelfService.Linux.Portal.Exceptions;
+
+namespace MultiFactor.SelfService.Linux.Portal.Filters
+{
+    public class FeatureNotEnabledExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger>();
+            var routeData = context.RouteData;
+            if(context.Exception is FeatureNotEnabledException ex)
+            {
+                var action = routeData.Values["action"] ?? "action";
+                var controller = routeData.Values["controller"] ?? "controller";
+                var route = $"/{controller}/{action}".ToLower();
+                logger.LogError("Unable to navigate to route '{r:l}' because required feature '{f:l}' is not enabled", route, ex.FeatureDescription);
+
+
+                context.HttpContext.Response.Clear();
+                context.HttpContext.Response.Redirect("~/home");
+            }
+        }
+    }
+}
