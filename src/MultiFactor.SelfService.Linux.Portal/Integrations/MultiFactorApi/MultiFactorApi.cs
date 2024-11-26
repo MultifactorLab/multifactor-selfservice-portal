de7ff2b648af7a03122b56a75042698e611733e3
Ivan-Timofeev <qwark4media@gmail.com>
2024-11-26 17:42:45 +0300

    Move 2fa configuration to acp application

--- src/MultiFactor.SelfService.Linux.Portal/Integrations/MultiFactorApi/MultiFactorApi.cs
+++ src/MultiFactor.SelfService.Linux.Portal/Integrations/MultiFactorApi/MultiFactorApi.cs
@@ -15,7 +15,7 @@ namespace MultiFactor.SelfService.Linux.Portal.Integrations.MultiFactorApi
 
         public MultiFactorApi(MultifactorHttpClientAdapterFactory clientFactory, HttpClientTokenProvider tokenProvider, PortalSettings settings)
         {
-            if (clientFactory is null) throw new ArgumentNullException(nameof(clientFactory));
+            ArgumentNullException.ThrowIfNull(clientFactory);
             _clientAdapter = clientFactory.CreateClientAdapter();
 
             _tokenProvider = tokenProvider ?? throw new ArgumentNullException(nameof(tokenProvider));
@@ -37,20 +37,19 @@ namespace MultiFactor.SelfService.Linux.Portal.Integrations.MultiFactorApi
 
             return ExecuteAsync(() => _clientAdapter.PostAsync<ApiResponse<BypassPageDto>>("access/bypass/saml", payload, GetBasicAuthHeaders()));
         }
-
+        
         /// <summary>
-        /// Removes specified authenticator from user profile.
+        /// Sends a request to create an enrollment request for the self-service portal.
         /// </summary>
-        /// <param name="authenticator">Name</param>
-        /// <param name="id">Id</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="UnsuccessfulResponseException"></exception>
-        public Task RemoveAuthenticatorAsync(string authenticator, string id)
+        /// <returns>
+        /// A task that represents the asynchronous operation, containing an <see cref="ApiResponse{EnrollmentPageDto}"/> object.
+        /// </returns>
+        public Task<ApiResponse<EnrollmentPageDto>> CreateEnrollmentRequest()
         {
-            if (authenticator is null)  throw new ArgumentNullException(nameof(authenticator));
-            if (id is null) throw new ArgumentNullException(nameof(id));
-
-            return ExecuteAsync(() => _clientAdapter.DeleteAsync<ApiResponse>($"self-service/{authenticator}/{id}", GetBearerAuthHeaders()));
+             return _clientAdapter.PostAsync<ApiResponse<EnrollmentPageDto>>(
+                 $"/self-service/create-enrollment-request?dcCode={_settings.CompanySettings.Domain}",
+                 data: null,
+                 GetBearerAuthHeaders());
         }
 
         /// <summary>
@@ -116,29 +115,6 @@ namespace MultiFactor.SelfService.Linux.Portal.Integrations.MultiFactorApi
             };
 
             return ExecuteAsync(() => _clientAdapter.PostAsync<ApiResponse<AccessPageDto>>("access/requests", payload, GetBasicAuthHeaders()));
-        }
-
-        /// <summary>
-        /// Returns new Time-based One Time Password.
-        /// </summary>
-        /// <exception cref="UnsuccessfulResponseException"></exception>
-        public Task<TotpKeyDto> CreateTotpKey() => ExecuteAsync(() => _clientAdapter.GetAsync<ApiResponse<TotpKeyDto>>("self-service/totp/new", GetBearerAuthHeaders()));
-
-        /// <summary>
-        /// Adds new Time-based One Time Password authenticator to the user profile.
-        /// </summary>
-        /// <param name="key">Totp identifier</param>
-        /// <param name="otp">Password</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="UnsuccessfulResponseException"></exception>
-        public Task AddTotpAuthenticatorAsync(string key, string otp)
-        {
-            if (key is null) throw new ArgumentNullException(nameof(key));
-            if (otp is null) throw new ArgumentNullException(nameof(otp));
-
-            var payload = new { key, otp };
-
-            return ExecuteAsync(() => _clientAdapter.PostAsync<ApiResponse>("self-service/totp", payload, GetBearerAuthHeaders()));
         }
 
         public Task<ResetPasswordDto> StartResetPassword(string identity, string callbackUrl)
