45da5027ba0d266333ac81576f9602733c8a0a02
i.suchkov <i.suchkov@multifactor.ru>
2025-05-12 14:43:22 +0500

    DEV-555 * добавлена проверка пароля для смены забытого пароля

--- src/MultiFactor.SelfService.Linux.Portal/Integrations/Ldap/PasswordChanging/ForgottenPasswordChanger.cs
+++ src/MultiFactor.SelfService.Linux.Portal/Integrations/Ldap/PasswordChanging/ForgottenPasswordChanger.cs
@@ -4,6 +4,7 @@ using MultiFactor.SelfService.Linux.Portal.Abstractions.Ldap;
 using MultiFactor.SelfService.Linux.Portal.Extensions;
 using MultiFactor.SelfService.Linux.Portal.Integrations.Ldap.Connection;
 using MultiFactor.SelfService.Linux.Portal.Integrations.Ldap.ProfileLoading;
+using MultiFactor.SelfService.Linux.Portal.Settings.PasswordRequirement;
 
 namespace MultiFactor.SelfService.Linux.Portal.Integrations.Ldap.PasswordChanging
 {
@@ -14,23 +15,32 @@ namespace MultiFactor.SelfService.Linux.Portal.Integrations.Ldap.PasswordChangin
         private readonly ILogger _logger;
         private readonly IStringLocalizer<SharedResource> _localizer;
         private readonly LdapProfileLoader _profileLoader;
+        private readonly PasswordRequirementsService _passwordRequirementsService;
 
 
         public ForgottenPasswordChanger(LdapConnectionAdapterFactory connectionFactory,
             ILogger<UserPasswordChanger> logger,
             IPasswordAttributeReplacer passwordAttributeReplacer,
             IStringLocalizer<SharedResource> localizer,
-            LdapProfileLoader profileLoader)
+            LdapProfileLoader profileLoader,
+            PasswordRequirementsService passwordRequirementsService)
         {
             _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
             _passwordAttributeReplacer = passwordAttributeReplacer ?? throw new ArgumentNullException(nameof(passwordAttributeReplacer));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
             _profileLoader = profileLoader ?? throw new ArgumentNullException(nameof(profileLoader));
+            _passwordRequirementsService = passwordRequirementsService;
         }
 
         public async Task<PasswordChangingResult> ChangePassword(string username,string newPassword)
         {
+            var validationResult = _passwordRequirementsService.ValidatePassword(newPassword);
+            if (!validationResult.IsValid)
+            {
+                _logger.LogWarning("Change/reset password for user '{username}' failed: {message:l}", username, validationResult);
+                return new PasswordChangingResult(false, validationResult.ToString());
+            }
             try
             {
                 using var connection = await _connectionFactory.CreateAdapterAsTechnicalAccAsync();
