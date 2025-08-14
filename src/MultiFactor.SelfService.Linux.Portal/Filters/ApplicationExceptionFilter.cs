4bf07787dce39d3db75e8897737b1922f3a17939
Sergey <42936486+Tu-S@users.noreply.github.com>
2025-08-14 15:44:04 +0700

    DEV-434 (#97)
    * DEV-434
    
    * обработка антифрод токена
    
    ---------
    
    Co-authored-by: Vladimir Zimin <v.zimin@multifactor.ru>

--- src/MultiFactor.SelfService.Linux.Portal/Filters/ApplicationExceptionFilter.cs
+++ src/MultiFactor.SelfService.Linux.Portal/Filters/ApplicationExceptionFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using MultiFactor.SelfService.Linux.Portal.Exceptions;
 
@@ -13,6 +14,13 @@ namespace MultiFactor.SelfService.Linux.Portal.Filters
                 context.ModelState.AddModelError(string.Empty, context.Exception.Message);
                 context.ExceptionHandled = true;
                 context.Result = new RedirectToActionResult("logout", "account", new { });
+            }
+
+            if (context.Exception is AntiforgeryValidationException ave)
+            {
+                context.ModelState.AddModelError(string.Empty, context.Exception.Message);
+                context.ExceptionHandled = true;
+                context.Result = new RedirectToActionResult("SessionExpired", "Error", new { });
             }
         }
     }
--- src/MultiFactor.SelfService.Linux.Portal/Program.cs
+++ src/MultiFactor.SelfService.Linux.Portal/Program.cs
@@ -6,82 +6,118 @@ using MultiFactor.SelfService.Linux.Portal.Filters;
 using MultiFactor.SelfService.Linux.Portal.ModelBinding;
 using MultiFactor.SelfService.Linux.Portal.ModelBinding.Binders;
 using System.Net;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    Start();
+}
+catch (Exception ex)
+{
+    var errorMessage = FlattenException(ex);
+    StartupLogger.Error(ex, "Unable to start: {Message:l}", errorMessage);
+}
 
-builder.AddConfiguration(args)
-    .ConfigureLogging()
-    .RegisterConfiguration()
-    .ConfigureApplicationServices()
-    .ConfigureAuthentication()
-    .AddControllersWithViewsAndLocalization(o =>
+void Start()
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.AddConfiguration(args)
+        .ConfigureLogging()
+        .RegisterConfiguration()
+        .ConfigureApplicationServices()
+        .ConfigureAuthentication()
+        .AddControllersWithViewsAndLocalization(o =>
+        {
+            o.ModelBinderProviders.Insert(0, ModelBindingConfiguration.GetModelBinderProvider());
+            o.Filters.Add<ApplicationExceptionFilter>();
+            o.Filters.Add<FeatureNotEnabledExceptionFilter>();
+        });
+
+    // "If data protection isn't configured, the keys are held in memory and discarded when the app restarts."
+    builder.Services.AddDataProtection()
+        .PersistKeysToFileSystem(new DirectoryInfo(Constants.KEY_STORAGE_DIRECTORY))
+        .SetApplicationName("MultiFactorSSPL");
+
+    var app = builder.Build();
+
+    app.UseForwardedHeaders(new ForwardedHeadersOptions
     {
-        o.ModelBinderProviders.Insert(0, ModelBindingConfiguration.GetModelBinderProvider());
-        o.Filters.Add<ApplicationExceptionFilter>();
-        o.Filters.Add<FeatureNotEnabledExceptionFilter>();
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
     });
 
-
-// "If data protection isn't configured, the keys are held in memory and discarded when the app restarts."
-builder.Services.AddDataProtection()
-    .PersistKeysToFileSystem(new DirectoryInfo(Constants.KEY_STORAGE_DIRECTORY))
-    .SetApplicationName("MultiFactorSSPL");
-
-var app = builder.Build();
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
-
-if (app.Environment.IsEnvironment(Constants.PRODUCTION_ENV))
-{
-    app.UseExceptionHandler("/error");
-}
-else
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseRequestLocalization();
-app.UseCookiePolicy();
-app.UseSession();
-
-app.UseStaticFiles();
-app.Use(async (context, next) =>
-{
-    var token = context.Request.Cookies[Constants.COOKIE_NAME];
-    if (!string.IsNullOrEmpty(token))
+    if (app.Environment.IsEnvironment(Constants.PRODUCTION_ENV))
     {
-        context.Request.Headers.Authorization = $"Bearer {token}";
+        app.UseExceptionHandler("/error");
+    }
+    else
+    {
+        app.UseDeveloperExceptionPage();
     }
 
-    await next();
-});
-app.UseStatusCodePages(context =>
-{
-    var request = context.HttpContext.Request;
-    if (request.IsAjaxCall()) return Task.CompletedTask;
+    app.UseRequestLocalization();
+    app.UseCookiePolicy();
+    app.UseSession();
 
-    var response = context.HttpContext.Response;
-    if (response.StatusCode == (int)HttpStatusCode.Unauthorized)
+    app.UseStaticFiles();
+    app.Use(async (context, next) =>
     {
-        response.Redirect($"/account/logout{MultiFactorClaimsDtoBinder.FromRequest(request)}");
+        var token = context.Request.Cookies[Constants.COOKIE_NAME];
+        if (!string.IsNullOrEmpty(token))
+        {
+            context.Request.Headers.Authorization = $"Bearer {token}";
+        }
+
+        await next();
+    });
+    app.UseStatusCodePages(context =>
+    {
+        var request = context.HttpContext.Request;
+        if (request.IsAjaxCall()) return Task.CompletedTask;
+
+        var response = context.HttpContext.Response;
+        if (response.StatusCode == (int)HttpStatusCode.Unauthorized)
+        {
+            response.Redirect($"/account/logout{MultiFactorClaimsDtoBinder.FromRequest(request)}");
+        }
+
+        return Task.CompletedTask;
+    });
+
+    app.UseAuthentication();
+    app.UseRouting();
+    app.UseAuthorization();
+
+    app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+    app.MapApiEndpoints();
+
+    app.Run();
+}
+
+string FlattenException(Exception exception)
+{
+    var stringBuilder = new StringBuilder();
+
+    var counter = 0;
+
+    while (exception != null)
+    {
+        if (counter++ > 0)
+        {
+            var prefix = new string('-', counter) + ">\t";
+            stringBuilder.Append(prefix);
+        }
+
+        stringBuilder.AppendLine(exception.Message);
+        exception = exception.InnerException;
     }
 
-    return Task.CompletedTask;
-});
-
-app.UseAuthentication();
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-app.MapApiEndpoints();
-
-app.Run();
+    return stringBuilder.ToString();
+}
 
 // Needs for tests.
 #pragma warning disable CA1050 // Declare types in namespaces
-public partial class Program {}
+public partial class Program
+{
+}
 #pragma warning restore CA1050 // Declare types in namespaces
