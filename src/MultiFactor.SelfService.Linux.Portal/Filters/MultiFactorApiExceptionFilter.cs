52d77ea66d856d596d29618db48bec904cafbeec
i.suchkov <i.suchkov@multifactor.ru>
2025-09-10 16:29:36 +0500

    added api exception filter to handle various 200 success false errors

---
+++ src/MultiFactor.SelfService.Linux.Portal/Filters/MultiFactorApiExceptionFilter.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MultiFactor.SelfService.Linux.Portal.Integrations.MultiFactorApi.Exceptions;
+
+namespace MultiFactor.SelfService.Linux.Portal.Filters
+{
+    public class MultiFactorApiExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<MultiFactorApiExceptionFilter> _logger;
+
+        public MultiFactorApiExceptionFilter(ILogger<MultiFactorApiExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is UnsuccessfulResponseException apiException)
+            {
+                var rd = context.HttpContext.GetRouteData();
+                var action = rd.Values["action"]?.ToString() ?? "action";
+                var controller = rd.Values["controller"]?.ToString() ?? "controller";
+                
+                _logger.LogWarning("MultiFactorApi error in {Controller}/{Action}: {Message:l}", 
+                    controller, action, apiException.Message);
+
+                var result = HandleByMessageContent(apiException.Message, context);
+
+                if (result == null) return;
+                
+                context.ExceptionHandled = true;
+                context.Result = result;
+            }
+        }
+
+        private IActionResult HandleByMessageContent(string message, ExceptionContext context)
+        {
+            context.ModelState.AddModelError(string.Empty, context.Exception.Message);
+            return message.ToLowerInvariant() switch
+            {
+                var msg when msg.Contains("unauthorized") || msg.Contains("access denied") || msg.Contains("usernotregistered") => 
+                    new RedirectToActionResult("AccessDenied", "Error", new { }),
+                
+                _ => new RedirectToActionResult("Index", "Error", new { })
+            };
+        }
+    }
+}
