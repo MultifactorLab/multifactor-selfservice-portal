bf6b22bdc001922360c31e38883dd5455da6b099
i.suchkov <i.suchkov@multifactor.ru>
2025-09-30 10:28:00 +0500

    resolve merge conflicts

--- src/MultiFactor.SelfService.Linux.Portal/Integrations/MultiFactorApi/IMultiFactorApi.cs
+++ src/MultiFactor.SelfService.Linux.Portal/Integrations/MultiFactorApi/IMultiFactorApi.cs
@@ -6,8 +6,8 @@ namespace MultiFactor.SelfService.Linux.Portal.Integrations.MultiFactorApi
     public interface IMultiFactorApi
     {
         Task PingAsync();
-        Task<BypassPageDto> CreateSamlBypassRequestAsync(string login, string samlSessionId);
-        Task<BypassPageDto> CreateOidcBypassRequestAsync(string login, string oidcSessionId);
+        Task<BypassPageDto> CreateSamlBypassRequestAsync(UserProfileDto user, string samlSessionId);
+        Task<BypassPageDto> CreateOidcBypassRequestAsync(UserProfileDto user, string oidcSessionId);
         Task<ResetPasswordDto> StartResetPassword(string twoFaIdentity, string ldapIdentity, string callbackUrl);
         Task<UnlockUserDto> StartUnlockingUser(string identity, string callbackUrl);
 
