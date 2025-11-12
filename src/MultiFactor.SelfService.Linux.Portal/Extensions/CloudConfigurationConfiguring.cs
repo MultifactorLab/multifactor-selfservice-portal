fea24d3f6c90e971f988b34c1ef1677d417326a7
s.agibalov <s.agibalov@multifactor.ru>
2025-11-12 07:45:41 +0300

    DEV-896: changed cloudConfig updating to backgroundService

--- src/MultiFactor.SelfService.Linux.Portal/Extensions/CloudConfigurationConfiguring.cs
+++ src/MultiFactor.SelfService.Linux.Portal/Extensions/CloudConfigurationConfiguring.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.Extensions.Caching.Memory;
-using MultiFactor.SelfService.Linux.Portal.Core.Configuration.Providers;
-using MultiFactor.SelfService.Linux.Portal.Integrations.MultiFactorApi;
+﻿using MultiFactor.SelfService.Linux.Portal.Options;
+using MultiFactor.SelfService.Linux.Portal.Services;
 using MultiFactor.SelfService.Linux.Portal.Settings;
 
 namespace MultiFactor.SelfService.Linux.Portal.Extensions
@@ -9,24 +8,12 @@ namespace MultiFactor.SelfService.Linux.Portal.Extensions
     {
         public static WebApplicationBuilder ConfigureCloudConfiguration(this WebApplicationBuilder builder)
         {
-            var provider = builder.Services.BuildServiceProvider();
-            var configProvider = new CloudConfigurationProvider(
-                provider.GetRequiredService<IMultiFactorApi>(),
-                provider.GetRequiredService<IMemoryCache>(),
-                provider.GetRequiredService<IWebHostEnvironment>(),
-                provider.GetRequiredService<ILogger<CloudConfigurationProvider>>());
-
-            builder.Host.ConfigureAppConfiguration((hostingContext, configurationBuilder) =>
-            {
-                configurationBuilder.Add(configProvider);
-            });
-
             builder.Services.AddOptions<ShowcaseSettings>()
                 .BindConfiguration("ShowcaseSettings")
                 .ValidateDataAnnotations();
 
-            builder.Services.AddSingleton(configProvider);
-            builder.Services.AddSingleton<ICloudConfigurationRefresher, CloudConfigurationRefresher>();
+            builder.Services.AddTransient<IShowcaseSettingsOptions, ShowcaseSettingsOptions>();
+            builder.Services.AddHostedService<ShowcaseSettingsUpdaterService>();
 
             return builder;
         }
---
+++ src/MultiFactor.SelfService.Linux.Portal/Services/ShowcaseSettingsUpdaterService.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using MultiFactor.SelfService.Linux.Portal.Integrations.MultiFactorApi;
+using MultiFactor.SelfService.Linux.Portal.Settings;
+
+namespace MultiFactor.SelfService.Linux.Portal.Services
+{
+    public class ShowcaseSettingsUpdaterService : BackgroundService
+    {
+        private readonly IMultiFactorApi _multifactorApi;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly IOptionsMonitorCache<ShowcaseSettings> _cache;
+        private readonly ILogger<ShowcaseSettingsUpdaterService> _logger;
+        private readonly TimeSpan _updatePeriod = TimeSpan.FromSeconds(90);
+
+        public ShowcaseSettingsUpdaterService(
+            IMultiFactorApi api,
+            IWebHostEnvironment hostEnvironment,
+            IOptionsMonitorCache<ShowcaseSettings> cache,
+            ILogger<ShowcaseSettingsUpdaterService> logger)
+        {
+            _multifactorApi = api;
+            _hostEnvironment = hostEnvironment;
+            _cache = cache;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken ct)
+        {
+            using PeriodicTimer timer = new PeriodicTimer(_updatePeriod);
+
+            while (!ct.IsCancellationRequested && await timer.WaitForNextTickAsync(ct))
+            {
+                try
+                {
+                    var settings = await _multifactorApi.GetShowcaseSettingsAsync();
+                    _cache.TryRemove("ShowcaseSettings");
+                    _cache.TryAdd("ShowcaseSettings", settings);
+                    await UpdateLogos(settings);
+
+                    _logger.LogInformation("Showcase settings loaded successfully");
+                }
+                catch (Exception ex)
+                {
+                    var test = ex.ToString();
+                    _logger.LogError(ex, "Failed to load Showcase settings");
+                }
+            }
+        }
+
+        private async Task UpdateLogos(ShowcaseSettings settings)
+        {
+            if (settings == null)
+            {
+                return;
+            }
+
+            var localFolder = Path.Combine(_hostEnvironment.WebRootPath, "content", "images", "showcase");
+            if (!Directory.Exists(localFolder))
+            {
+                Directory.CreateDirectory(localFolder);
+            }
+
+            var cloudFileNames = settings.Links.Select(x => x.Image).ToArray();
+            var localFileNames = Directory.GetFiles(localFolder)
+                .Select(file => Path.GetFileName(file))
+                .ToArray();
+
+            var missingFiles = cloudFileNames.Except(localFileNames).ToArray();
+            foreach (var fileName in missingFiles)
+            {
+                try
+                {
+                    var data = await _multifactorApi.GetShowcaseLogoAsync(fileName);
+                    if (data is null)
+                    {
+                        continue;
+                    }
+
+                    File.WriteAllBytes(Path.Combine(localFolder, fileName), data);
+                }
+                catch (HttpRequestException) { }
+            }
+
+            var extraFiles = localFileNames.Except(cloudFileNames).ToArray();
+            foreach (var filename in extraFiles)
+            {
+                File.Delete(Path.Combine(localFolder, filename));
+            }
+        }
+    }
+}
